31f33680f0e13e6ff76fdd7419b4d33355000f69
Dmytro Palii <dpalii.study@gmail.com>
2023-05-29 20:55:21 +0300

    implementation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# pipe-filter
+# Pipe-filter
+
+In this example, we have an IFilter\<T> interface that represents a filter in the Pipe-Filter pattern. Each filter implements this interface and processes the data according to its specific logic.
+
+Two example filters are implemented: UppercaseFilter, which converts the input strings to uppercase, and LengthFilter, which appends the length of each input string to the result.
+
+The Pipeline\<T> class represents the pipeline that connects the filters together. It allows adding filters to the pipeline and executing the data through each filter in sequence.
+
+In the Program class, we create instances of the filters (uppercaseFilter and lengthFilter) and add them to the pipeline. We provide input data (inputData) as a list of strings. We then execute the pipeline using the Execute method, passing in the input data. The result is obtained and printed to the console.
+
+When executed, the program will process the input data through each filter in the pipeline, resulting in the desired transformations being applied to the data.
+
+This example demonstrates how the Pipe-Filter pattern can be used to create modular and reusable processing components, allowing for flexible and scalable data processing pipelines.
--- pipe-filter/Program.cs
+++ pipe-filter/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace YourNamespace
+{
+    // Interface representing a filter in the Pipe-Filter pattern
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Process(IEnumerable<T> input);
+    }
+
+    // Example filter implementation
+    public class UppercaseFilter : IFilter<string>
+    {
+        public IEnumerable<string> Process(IEnumerable<string> input)
+        {
+            foreach (string item in input)
+            {
+                yield return item.ToUpper();
+            }
+        }
+    }
+
+    // Example filter implementation
+    public class LengthFilter : IFilter<string>
+    {
+        public IEnumerable<string> Process(IEnumerable<string> input)
+        {
+            foreach (string item in input)
+            {
+                yield return $"Length: {item.Length}";
+            }
+        }
+    }
+
+    // Pipeline class that connects filters together
+    public class Pipeline<T>
+    {
+        private readonly List<IFilter<T>> _filters = new List<IFilter<T>>();
+
+        public void AddFilter(IFilter<T> filter)
+        {
+            _filters.Add(filter);
+        }
+
+        public IEnumerable<T> Execute(IEnumerable<T> input)
+        {
+            IEnumerable<T> output = input;
+
+            foreach (IFilter<T> filter in _filters)
+            {
+                output = filter.Process(output);
+            }
+
+            return output;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create filters
+            var uppercaseFilter = new UppercaseFilter();
+            var lengthFilter = new LengthFilter();
+
+            // Create pipeline
+            var pipeline = new Pipeline<string>();
+            pipeline.AddFilter(uppercaseFilter);
+            pipeline.AddFilter(lengthFilter);
+
+            // Input data
+            var inputData = new List<string> { "hello", "world" };
+
+            // Execute pipeline
+            IEnumerable<string> result = pipeline.Execute(inputData);
+
+            // Output results
+            foreach (string item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
